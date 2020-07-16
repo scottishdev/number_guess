718ed8308ebab02cb5ace534bc4eef572d4b5343
Tartan Developer <pchu192@gmail.com>
2020-07-16 17:29:34 +0100

    button works

--- src/main/java/pcodeclan/com/numberguess/MainActivity.java
+++ src/main/java/pcodeclan/com/numberguess/MainActivity.java
@@ -3,8 +3,14 @@ package pcodeclan.com.numberguess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void guessFunction(View view){
+        Log.i("Works", "It works!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
