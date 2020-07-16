c793c623f4ac2f7a8dd515701a31b0b30cae30d2
Tartan Developer <pchu192@gmail.com>
2020-07-16 17:37:41 +0100

    random num works

--- src/main/java/pcodeclan/com/numberguess/MainActivity.java
+++ src/main/java/pcodeclan/com/numberguess/MainActivity.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    int randomNumber;
+
     public void guessFunction(View view){
         Log.i("Works", "It works!");
+        Log.i("Random number", String.valueOf(randomNumber));
+
+        EditText numberEnter = (EditText) findViewById(R.id.numberEnterEditTextNumber);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+
+        randomNumber = rand.nextInt(20)+1;
     }
 }
